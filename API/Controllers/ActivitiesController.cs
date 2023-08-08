0f92b3803a4ef81ef1048455a3f176a5baa6774f
yhaappdev <yha.appdev@gmail.com>
2023-08-08 17:23:32 +0800

    create activity handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {       
-    [AllowAnonymous]
     public class ActivitiesController : BaseApiController
     {
         [HttpGet] //api/activities
