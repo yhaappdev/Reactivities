c1d21d7933eb4447a44b35c204a8b442a47878b8
yhaappdev <yha.appdev@gmail.com>
2023-09-06 17:09:23 +0800

    End of section 16

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Identity;
-using System.Collections.Generic;
 
 namespace Domain
 {
@@ -8,5 +7,6 @@ namespace Domain
         public string DisplayName { get; set; }
         public string Bio { get; set; }
         public ICollection<ActivityAttendee> Activities { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
